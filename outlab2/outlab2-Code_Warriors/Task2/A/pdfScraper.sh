f41b7d51ea2ac4ee9d0393a22d463f5ae23fbb1d
gprince349 <48962985+gprince349@users.noreply.github.com>
2019-08-11 01:52:47 +0530

    Add files via upload

--- outlab2/outlab2-Code_Warriors/Task2/A/pdfScraper.sh
+++ outlab2/outlab2-Code_Warriors/Task2/A/pdfScraper.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 wget $1 -O tempfile.pdf
-pdftotext tempfile.pdf studentData.txt
+pdftotext tempfile.pdf temp.txt
+
 rm tempfile.pdf
 
+
+awk 'BEGIN {
+			FS="[\n]"
+			RS="--------------------------------------------------------------------\n\n"
+		    print "--------------------------------------------------------------------\n";
+			
+}
+{			
+			if (NR > 1) {
+				s = "" ;
+				for( i = 5; i <= NF ; i++)
+				{s = s $i " "; }
+				{print  $1;}
+				{print  $2;}
+				{print  $3;}
+				{print $4;}
+				{print  s;}
+				{print "--------------------------------------------------------------------\n";}
+			}
+}' temp.txt > studentData.txt
+
+rm temp.txt
--- outlab2/outlab2-Code_Warriors/Task2/C/sortStudentData.sh
+++ outlab2/outlab2-Code_Warriors/Task2/C/sortStudentData.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#sort -t'|' -k3  studentData.csv > sortedStudentData.csv
 
-awk 'NR<2{ print $0;next}{print $0| "sort -t\| -n -k3 " }' studentData.csv > sortedStudentData.csv
-
-awk 'NR==2,NR==6 {print $1}' sortedStudentData.csv > top5Students.txt
+sed -n -e '1 p' $1 > sortedStudentData.csv
+sed -n -e '2,$ p' $1 | sort -rk3,3 -t\| -V >> sortedStudentData.csv
+awk -F"|" 'NR==2,NR==6 {print $1}' sortedStudentData.csv > top5Students.txt
 
