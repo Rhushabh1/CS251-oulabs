76c79fb846404aaa7a774f74e0df10536bea3d4c
Rhushabh Gedam <rhushabhgedam3@gmail.com>
2019-09-10 12:13:36 +0530

    added task 2

--- outlab4/resources_outlab4/task2/task2.py
+++ outlab4/resources_outlab4/task2/task2.py
@@ -3,9 +3,10 @@ import numpy as np
 def add_patch(big, small, x,y):
 	br, bc = big.shape
 	sr,sc = small.shape
-	big[x:x+sr, y:y+sc] += small
-	# print(big)
-	return big
+	result = big.copy()
+	result[x:x+sr, y:y+sc] += small
+	
+	return result
 
 
 def reconstruct_from_noisy_patches(input_dict, shape):
@@ -23,27 +24,32 @@ def reconstruct_from_noisy_patches(input_dict, shape):
         
         bc = patch.copy()
         bc[patch==0] =1
+        bc[patch!=0] =0
         black_count = add_patch(black_count, bc, tlr, tlc)
 
         wc = patch.copy()
         wc[patch==255] =1
+        wc[patch!=255] =0
         white_count = add_patch(white_count, wc, tlr, tlc)        
 
         mc = patch.copy()
         
         mc[(patch!=0) & (patch!=255)] =1
+        mc[(patch==0) ^ (patch==255)] =0
         mid_count = add_patch(mid_count, mc, tlr, tlc)
 
-        mid_total = add_patch(mid_total, patch, tlr, tlc)
+        mt = patch.copy()
+        mt[(patch==0) ^ (patch==255)] =0
+        mid_total = add_patch(mid_total, mt, tlr, tlc)
 
 
-        
+    
+    M[(mid_count==0) & (black_count<=white_count)]= 255
+    M[(mid_count==0) & (black_count>white_count)] = 0
+    M[mid_count!=0] = np.around(mid_total[mid_count!=0]/mid_count[mid_count!=0])
 
-        M[mid_count!=0] = mid_total[mid_count!=0]/mid_count[mid_count!=0]
-
-        M[(mid_count==0) & (black_count<=white_count)]= 255
-        M[(mid_count==0) & (black_count>white_count)] = 0
+    M[(mid_count==0) & (white_count==0) & (black_count==0)]= 0
+    
 
     
     return M 
-
