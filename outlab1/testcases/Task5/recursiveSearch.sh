b70401ec5ed83366d4e5d8e9b1501ff4a0dfaccf
gprince349 <gprince349@gmail.com>
2019-09-24 20:14:58 +0530

    final outlab 5

--- outlab1/testcases/Task5/recursiveSearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ "$#" -eq 0 ]]
-then
-	echo "Usage: ./recursiveSearch.sh <words-to-search>"
-	exit 1
-fi
-# echo $@ are the inputs
-grep -inrw $@ Data/ 
--- outlab1/testcases/Task5/testcase2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./recursiveSearch.sh "the" "and" | sort > tempfile
-diff tempfile output2
-rm tempfile
-
-
-
-
